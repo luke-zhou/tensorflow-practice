2e4dbad6b340f3032b668f94076ce915615ca131
Luke <sharlock@gmail.com>
2018-03-18 18:48:29 +1100

    script files

---
+++ practice1/main.py
@@ -0,0 +1,61 @@
+import review_data
+import tensorflow as tf
+
+def main():
+
+    # Fetch the data
+    (train_x, train_y), (test_x, test_y) = review_data.load_data()
+
+    # Feature columns describe how to use the input.
+    my_feature_columns = []
+    for key in train_x.keys():
+        my_feature_columns.append(tf.feature_column.numeric_column(key=key))
+
+    # Build 2 hidden layer DNN with 10, 10 units respectively.
+    classifier = tf.estimator.DNNClassifier(
+        feature_columns=my_feature_columns,
+        # Two hidden layers of 10 nodes each.
+        hidden_units=[500, 250],
+        # The model must choose between 3 classes.
+        n_classes=2)
+
+    # Train the Model.
+    classifier.train(
+        input_fn=lambda:review_data.train_input_fn(train_x, train_y,
+                                                 7000),
+        steps=1000)
+
+    # Evaluate the model.
+    eval_result = classifier.evaluate(
+        input_fn=lambda:review_data.eval_input_fn(test_x, test_y,
+                                                500))
+
+    print('\nTest set accuracy: {accuracy:0.3f}\n'.format(**eval_result))
+
+    # Generate predictions from the model
+    # expected = ['Setosa', 'Versicolor', 'Virginica']
+    # predict_x = {
+    #     'SepalLength': [5.1, 5.9, 6.9],
+    #     'SepalWidth': [3.3, 3.0, 3.1],
+    #     'PetalLength': [1.7, 4.2, 5.4],
+    #     'PetalWidth': [0.5, 1.5, 2.1],
+    # }
+
+
+    # predictions = classifier.predict(
+    #     input_fn=lambda:iris_data.eval_input_fn(predict_x,
+    #                                             labels=None,
+    #                                             batch_size=args.batch_size))
+                                                  
+    # for pred_dict, expec in zip(predictions, expected):
+    #     template = ('\nPrediction is "{}" ({:.1f}%), expected "{}"')
+
+    #     class_id = pred_dict['class_ids'][0]
+    #     print(pred_dict['class_ids'])
+    #     probability = pred_dict['probabilities'][class_id]
+
+    #     print(template.format(iris_data.SPECIES[class_id],
+    #                           100 * probability, expec))
+
+if __name__ == '__main__':
+    main()
---
+++ practice1/review_data.py
@@ -0,0 +1,110 @@
+from nltk.tokenize import RegexpTokenizer
+from nltk.stem import WordNetLemmatizer
+from collections import Counter
+import pandas as pd
+import random
+import tensorflow as tf
+
+
+def load_data():
+    words = [];
+    words += load_file('data-set/rt-polarity.neg')
+    words += load_file('data-set/rt-polarity.pos')
+    
+    features = generate_features(words)
+    print(len(features))
+
+    features_data = generate_features_date(features)
+
+    data_size = len(features_data)
+    random.shuffle(features_data)
+    test_data_size = int(data_size/10)
+    test_features_data = features_data[:test_data_size]
+    training_features_data = features_data[test_data_size:]
+
+    features.append('tf_classification_type')
+    print(features)
+    test_data_frame = pd.DataFrame(test_features_data, columns=features)
+    traning_data_frame = pd.DataFrame(training_features_data, columns=features)
+
+    print(traning_data_frame.head())
+    # print(traning_data_frame.keys())
+    train_x, train_y = traning_data_frame, traning_data_frame.pop('tf_classification_type')
+    test_x, test_y = test_data_frame, test_data_frame.pop('tf_classification_type')
+    print(train_y)
+    return (train_x, train_y), (test_x, test_y)
+
+def test():
+    temp =[[1,2,3],[4,5,6],[7,8,9]]
+    column =['a', 'b', 'c']
+    df = pd.DataFrame(temp,columns=column)
+    print(df)
+
+    temp2 = [1,2,3,4,5,6,7,8,9,10]
+    print(temp2[:3])
+    print(temp2[3:])
+
+def generate_features_date(features):
+    def review_to_feature_vector(review, type):
+        tokenizer = RegexpTokenizer(r'[A-Za-z0-9.][A-Za-z0-9_.]*')
+        lemmatizer = WordNetLemmatizer()
+        words = [lemmatizer.lemmatize(word) for word in tokenizer.tokenize(review)]
+        feature_vector = [1 if feature in words else 0 for feature in features]
+        feature_vector.append(type)
+        return feature_vector
+    
+    features_data =[]
+    with open('data-set/rt-polarity.neg','r') as f:
+        lines = [line.lower() for line in f]
+        features_data += [review_to_feature_vector(line,0) for line in lines]
+    with open('data-set/rt-polarity.pos','r') as f:
+        lines = [line.lower() for line in f]
+        features_data += [review_to_feature_vector(line,1) for line in lines]
+    
+    return features_data
+
+
+def generate_features(words):
+    total_word_num = len(words)
+    words_count = Counter(words)
+    features = [word for word in words_count if words_count[word]>total_word_num/10000 and words_count[word]<total_word_num/100]
+    return features
+
+def load_file(file_name):
+    with open(file_name,'r') as f:
+        tokenizer = RegexpTokenizer(r'[A-Za-z0-9.][A-Za-z0-9_.]*')
+        lines = [line.lower() for line in f]
+        words = sum([tokenizer.tokenize(line) for line in lines], [])
+        lemmatizer = WordNetLemmatizer()
+        lemmatized_words = [lemmatizer.lemmatize(word) for word in words]
+        return lemmatized_words
+
+def train_input_fn(features, labels, batch_size):
+    """An input function for training"""
+    # Convert the inputs to a Dataset.
+    dataset = tf.data.Dataset.from_tensor_slices((dict(features), labels))
+
+    # Shuffle, repeat, and batch the examples.
+    dataset = dataset.shuffle(10000).repeat().batch(batch_size)
+
+    # Return the dataset.
+    return dataset
+
+def eval_input_fn(features, labels, batch_size):
+    """An input function for evaluation or prediction"""
+    features=dict(features)
+    if labels is None:
+        # No labels, use only features.
+        inputs = features
+    else:
+        inputs = (features, labels)
+
+    # Convert the inputs to a Dataset.
+    dataset = tf.data.Dataset.from_tensor_slices(inputs)
+
+    # Batch the examples
+    assert batch_size is not None, "batch_size must not be None"
+    dataset = dataset.batch(batch_size)
+
+    # Return the dataset.
+    return dataset
