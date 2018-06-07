c71833626f3bec9a9e0f137c9ef21e25aeebbde9
Luke <sharlock@gmail.com>
2018-06-07 21:56:05 +1000

    update result file

--- lottery/update_lotto_result.py
+++ lottery/update_lotto_result.py
@@ -1,6 +1,8 @@
 import urllib.request
 import json  
 from io import StringIO
+import csv
+from datetime import datetime
 
 def get_result(min_draw, max_draw):
     post_body = {'MinDrawNo': min_draw, 'MaxDrawNo': max_draw, 'Product': "OzLotto", 'CompanyFilter': ["Tattersalls"]}
@@ -15,9 +17,39 @@ def get_result(min_draw, max_draw):
     response = urllib.request.urlopen(req, json_data_bytes)
     result = json.load(StringIO(response.read().decode("utf-8")))
     print(len(result['Draws']))
+    return result['Draws']
 
-    for draw in result['Draws']:
-        print(draw)
+def update_result_file():
+    missing_results = get_result(1046, 1060)
+    missing_results.reverse()
+    with open('resource/test.csv', 'a', newline='') as csvfile:
+        result_file = csv.writer(csvfile, delimiter=',')
+        for result in missing_results:
+            draw_date = datetime.strptime(result['DrawDate'], '%Y-%m-%dT%H:%M:%S')
+            result_file.writerow([result['DrawNumber'], draw_date.strftime('%Y%m%d')]+result['PrimaryNumbers']+result['SecondaryNumbers'])
+    csvfile.close()
+
+def update_ozlotto_latest_result():
+    missing_results = []
+    part_results = get_result(1046, 1095)
+    missing_results.extend(part_results)
+    part_results = get_result(1096, 1145)
+    missing_results.extend(part_results)
+    part_results = get_result(1146, 1195)
+    missing_results.extend(part_results)
+    part_results = get_result(1196, 1245)
+    missing_results.extend(part_results)
+    part_results = get_result(1246, 1269)
+    missing_results.extend(part_results)
+
+    missing_results.reverse()
+    print(len(missing_results))
+    with open('resource/Ozlotto-latest.csv', 'a', newline='') as csvfile:
+        result_file = csv.writer(csvfile, delimiter=',')
+        for result in missing_results:
+            draw_date = datetime.strptime(result['DrawDate'], '%Y-%m-%dT%H:%M:%S')
+            result_file.writerow([result['DrawNumber'], draw_date.strftime('%Y%m%d')]+result['PrimaryNumbers']+result['SecondaryNumbers'])
+    csvfile.close()   
 
 if __name__ == '__main__':
-    get_result(1199, 1200)
+    update_ozlotto_latest_result()
