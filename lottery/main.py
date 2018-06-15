70524509ed8e938eddde59151cea2c6f9e417a3b
luke zhou <sharlock@gmail.com>
2018-06-15 16:46:14 +1000

    change feature type to category

--- lottery/main.py
+++ lottery/main.py
@@ -1,8 +1,9 @@
 import lotto_data
 import tensorflow as tf
+import csv
 
 
-def main(file_name):
+def main(file_name, num):
     (train_x, train_y), (test_x, test_y) = lotto_data.load_data(file_name)
     # print(train_x)
     # print(train_y)
@@ -10,7 +11,14 @@ def main(file_name):
     # print('x-key')
     # print(train_x.keys())
     for key in train_x.keys():
-        my_feature_columns.append(tf.feature_column.numeric_column(key=key))
+        if key == 'draw_number' or key == 'date':
+            my_feature_columns.append(
+                tf.feature_column.numeric_column(key=key))
+        else:
+            feature_column = tf.feature_column.categorical_column_with_identity(
+                key=key, num_buckets=46)
+            my_feature_columns.append(
+                tf.feature_column.indicator_column(feature_column))
     # print('feature')
     # print(my_feature_columns)
     # Build 2 hidden layer DNN with 10, 10 units respectively.
@@ -56,6 +64,12 @@ def main(file_name):
         probability = prediction['probabilities'][class_id]
         print(template.format(lotto_data.RESULTS[class_id], 100 * probability))
         print('')
+        with open('output/result.csv', 'a', newline='') as data_csv_file:
+            data_file = csv.writer(data_csv_file, delimiter=',')
+            row = [num]
+            row.extend(prediction['probabilities'])
+            data_file.writerow(row)
+        data_csv_file.close()
 
     # template = ('\nPrediction is "{}" ({:.1f}%), expected "{}"')
     # for pred_dict, expec in zip(predictions, expected):
@@ -68,4 +82,4 @@ def main(file_name):
 
 if __name__ == '__main__':
     for i in range(1, 46):
-        main('resource/preprocess-data-'+str(i)+'.csv')
+        main('resource/preprocess-data-'+str(i)+'.csv', i)
