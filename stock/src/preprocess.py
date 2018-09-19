4d4433b31d49bd083eb494c5df071ff38ec93832
Luke <sharlock@gmail.com>
2018-09-19 20:16:51 +1000

    update preprocessing

--- stock/src/preprocess.py
+++ stock/src/preprocess.py
@@ -1,45 +1,14 @@
 import csv
-from datetime import datetime
 
-tracing_back_count = 10
-
-
-def preproces_one_num(csv_file, test_num):
-    test_number = str(test_num)
-    with open(csv_file, newline='') as csvfile:
-        csv_reader = csv.reader(csvfile, delimiter=',')
-        results = [row for row in csv_reader]
-        # print(results)
-        with open('resource/preprocess-data-'+test_number+'.csv', 'w', newline='') as data_csv_file:
-            data_file = csv.writer(data_csv_file, delimiter=',')
-            data_file.writerow(lotto_data.CSV_COLUMN_NAMES)
-
-            for i in range(tracing_back_count, len(results)):
-                row = [results[i][0]]
-                row.extend(generate_date_entry(results[i][1]))
-
-                # adding tracing back draws
-                for j in range(10, 0, -1):
-                    row.extend(results[i-j][2:11])
-                row.append(generate_result_category(test_number, results[i]))
-                data_file.writerow(row)
-        data_csv_file.close()
-
-
-def generate_date_entry(date_str):
-    entries = [date_str]
-    draw_datetime = datetime.strptime(date_str, '%Y%m%d')
-    draw_date = draw_datetime.date()
-    entries.append(draw_date.year)
-    entries.append(draw_date.month)
-    entries.append(draw_date.day)
-    return entries
+tracing_back_count = 5
 
 def preproces(file_name):
     with open(file_name) as csv_file:
         csv_reader = csv.reader(csv_file, delimiter=',')
         results = [row for row in csv_reader]
-        print(results)
+        for group in nGroup(results, tracing_back_count):
+            if group is not None:
+                print(group)
 
 def nGroup(list, n):
     for i in range(len(list)):
@@ -47,8 +16,10 @@ def nGroup(list, n):
             yield None
         else:
             result = list[i:i+n]
-            yield None if any(e is None for e in result) else result
+            yield result if isGroupValid(result) else None
 
+def isGroupValid(list):
+    return not any(any(e=='null' for e in r) for r in list)
 
 if __name__ == '__main__':
     preproces('../data/test-data.csv')
