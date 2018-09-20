2baf573a50c3c0b30b22035bb6a6dec2231828dc
Luke <sharlock@gmail.com>
2018-09-20 20:27:19 +1000

    generate features in tranning data

--- stock/src/preprocess.py
+++ stock/src/preprocess.py
@@ -3,12 +3,28 @@ import csv
 tracing_back_count = 5
 
 def preproces(file_name):
+    group_records =[]
     with open(file_name) as csv_file:
         csv_reader = csv.reader(csv_file, delimiter=',')
         results = [row for row in csv_reader]
-        for group in nGroup(results, tracing_back_count):
+        for group in nGroup(results, tracing_back_count+1):
             if group is not None:
-                print(group)
+                group_records.append(group)
+    csv_file.close()
+    with open('../training_data/preprocess-'+ str(tracing_back_count) +'.csv', 'w', newline='') as output_csv_file:
+        writer = csv.writer(output_csv_file, delimiter=',')
+        for group in group_records:
+            features =[]
+            for i in range(tracing_back_count):
+                for num in group[i]:
+                    try:
+                        v = float(num)
+                        features.append(v)
+                    except ValueError:
+                        pass
+            writer.writerow(features)
+
+    output_csv_file.close()
 
 def nGroup(list, n):
     for i in range(len(list)):
