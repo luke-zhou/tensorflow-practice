0065632e65398374533f29d7e24c3fa32a1661b3
Luke <sharlock@gmail.com>
2019-07-02 21:47:46 +1000

    add oz_generator

--- lottery/oz_generator.py
+++ lottery/oz_generator.py
@@ -1,9 +1,10 @@
 import random
 import time
 
-def random_gen(prefill =[] ):
-    millis = int(round(time.time() * 1000))
-    random.seed(a = millis)
+millis = int(round(time.time() * 1000))
+random.seed(a = millis)
+
+def random_set(prefill =[]):
     result = set(prefill)
     while len(result) < 7:
         num = random.randint(1, 45)
@@ -11,7 +12,16 @@ def random_gen(prefill =[] ):
     
     return sorted(list(result))
 
+def random_ticket(size):
+    return [random_set() for i in range(size)]
+
+def random_ticket_each():
+    return [random_set([i+1]) for i in range(45)]
+
 if __name__=='__main__':
-    print(random_gen())
-    print(random_gen([1]))
-    print(random_gen([2,3,4,5]))
+    print(random_set())
+    print(random_set([1]))
+    print(random_set([2,3,4,5]))
+
+    print(random_ticket(10))
+    print(random_ticket_each())
